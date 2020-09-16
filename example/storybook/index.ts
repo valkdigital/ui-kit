b71fb10835edd3033d6374de0a7288b2dfeea88a
Lars Timmerman <infigoforza@gmail.com>
2020-09-16 10:42:02 +0200

    @ts ignore for build

--- example/storybook/index.ts
+++ example/storybook/index.ts
@@ -8,7 +8,9 @@ configure(() => {
 
 // Refer to https://github.com/storybookjs/storybook/tree/master/app/react-native#start-command-parameters
 // To find allowed options for getStorybookUI
+
 const StorybookUIRoot = getStorybookUI({
+  // @ts-ignore
   asyncStorage: AsyncStorage,
 });
 
