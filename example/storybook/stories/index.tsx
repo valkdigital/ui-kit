07fb13c57c3a4ef501b0191aa6db2356f85ff02f
Lars Timmerman <infigoforza@gmail.com>
2020-09-16 11:48:38 +0200

    add Text to storybook app

--- example/storybook/stories/index.tsx
+++ example/storybook/stories/index.tsx
@@ -208,6 +208,29 @@ inputStories.add("Picker", () => (
   </CenteredView>
 ));
 
+storiesOf("Text").add("All Text", () => (
+  <CenteredView>
+    <Text type="h1">Heading 1</Text>
+    <Text type="h2">Heading 2</Text>
+    <Text type="h3">Heading 3</Text>
+    <Text type="h4">Heading 4</Text>
+    <Text type="h5">Heading 5</Text>
+    <Text type="h6">Heading 6</Text>
+    <Text type="subHeading">Sub Heading</Text>
+    <Text>{"\n"}</Text>
+    <Text>{"\n"}</Text>
+    <Text type="bodyRegular">Body - Regular</Text>
+    <Text type="bodySemiBold">Body Semi - Bold</Text>
+    <Text type="bodyItalic">Body Italic</Text>
+    <Text>{"\n"}</Text>
+    <Text>{"\n"}</Text>
+    <Text type="subtextRegular">Subtext - Regular</Text>
+    <Text type="subtextSemiBold">Subtext - Semibold</Text>
+    <Text type="subtextBold">subtextBold - Bold</Text>
+    <Text type="label">Label</Text>
+  </CenteredView>
+));
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
