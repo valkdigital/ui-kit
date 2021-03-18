4225358967fe0b55a79d1538e9115dedf04609e5
Misja <misja.vandekamp@redhotminute.com>
2021-03-18 15:34:37 +0100

    Iconset: initial commit

--- example/babel.config.js
+++ example/babel.config.js
@@ -5,7 +5,7 @@ module.exports = function (api) {
   api.cache(true);
 
   return {
-    presets: ["module:metro-react-native-babel-preset"],
+    presets: ["babel-preset-expo"],
     plugins: [
       [
         "module-resolver",
